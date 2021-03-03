b2d1e09ddc7a9e5291b654075311e311280672f5
Taylor Masiello <77504543+taylormasiello@users.noreply.github.com>
2021-03-03 10:12:48 -0800

    Uploaded completed Nucampsite Bootstrap Project
    Bootstrap module project from Nucamp coding bootcamp (completed)

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function() {
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+}); 
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+});
+
